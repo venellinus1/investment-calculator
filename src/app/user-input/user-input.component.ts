81251806e31c185a4f0faf31198b1facafe917ef
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-06 16:46:59 +0200

    Add Investment-Results Component

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Output  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from "../investment-input.model";
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';  
+
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
+  }
+}
