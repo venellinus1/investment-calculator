f259f582e72bcbd68f32165f40a530b4fabdb008
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-07 10:33:48 +0200

    Change user-input to use signals, replace EventEmitter with output() method

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output  } from '@angular/core';
+import { Component, EventEmitter, Output, signal, output  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import type { InvestmentInput } from "../investment-input.model";
 
@@ -10,22 +10,19 @@ import type { InvestmentInput } from "../investment-input.model";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<InvestmentInput>();
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';  
+  //@Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');  
 
   onSubmit(){
     this.calculate.emit({
-      initialInvestment: +this.enteredInitialInvestment,
-      duration: +this.enteredDuration,
-      expectedReturn: +this.enteredExpectedReturn,
-      annualInvestment: +this.enteredAnnualInvestment
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
     });
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
   }
 }
