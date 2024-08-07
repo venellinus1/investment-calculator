3ebaf2cbef7e463f41c5aae81ab57cbf4808199d
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-07 11:02:23 +0200

    Refactoring user-input: -inject InvestmentService -remove output -onSubmit - call investmentService method with the params, replacing the emit

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output, signal, output  } from '@angular/core';
+import { Component, signal  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from "../investment-input.model";
+import { InvestmentService } from './investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,20 +10,21 @@ import type { InvestmentInput } from "../investment-input.model";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  //@Output() calculate = new EventEmitter<InvestmentInput>();
-  calculate = output<InvestmentInput>();
+
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');  
 
+  constructor(private investmentService: InvestmentService){}
   onSubmit(){
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
       annualInvestment: +this.enteredAnnualInvestment()
     });
+   
     this.enteredAnnualInvestment.set('0');
     this.enteredAnnualInvestment.set('0');
     this.enteredExpectedReturn.set('5');
