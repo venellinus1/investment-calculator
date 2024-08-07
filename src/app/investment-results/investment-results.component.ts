d588bea123e42007a6aa39fd04507292ab2a0fbf
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-07 14:16:41 +0200

    Refactoring investment-results: -remove input() -inject investment service -modify html template to not use signal and back to use property for the results

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, input} from '@angular/core';
 import { InvestmentInput } from '../investment-input.model';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../user-input/investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -10,20 +11,9 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results = input<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[]>();
-  /*@Input() results?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[];*/
+  constructor(private investmentService: InvestmentService){}
+
+  get results(){
+    return this.investmentService.resultsData;
+  }
 }
