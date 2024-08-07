a962b7592aecb540c7ff943d4e1627ae58b5d1c8
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-07 16:07:50 +0200

    Refactoring towards use of Modules - the App now contains two comModules - App module and UserInputModule Created UserInputModule to replace the component. UserInputModule exports the UserInputComponent so that it is available to other non-Standalone components in the App
    App.module - now imports the UserInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserInputModule } from './user-input/user-input.module';
 
 @NgModule({
-    imports: [FormsModule, BrowserModule],
+    imports: [BrowserModule, UserInputModule],
     declarations: [
         AppComponent, 
         HeaderComponent, 
-        UserInputComponent, 
         InvestmentResultsComponent
     ],
     bootstrap:[AppComponent]
