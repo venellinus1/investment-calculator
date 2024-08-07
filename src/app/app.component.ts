ff65ed988efef5816b406d4932f0caae704bd72d
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-08-07 10:24:59 +0200

    Use signal to update resultsData in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from "./investment-input.model";
@@ -10,14 +10,22 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  resultsData?: {
+  /*resultsData?: {
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number,
-  }[];
+  }[];*/
+  resultsData = signal<{
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[] | undefined>(undefined);
   onCalculateInvestmentResults(data: InvestmentInput) {
     const annualData = [];
     const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
@@ -38,7 +46,8 @@ export class AppComponent {
         totalAmountInvested: initialInvestment + annualInvestment * year,
       });
     }
-    this.resultsData = annualData;
+    //this.resultsData = annualData;
+    this.resultsData.set(annualData);
   }
   
 }
